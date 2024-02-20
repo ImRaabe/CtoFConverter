3b04f65ff509d7d238dc8e96664328949731bf3a
ImRaabe <raabees@mail.uc.edu>
2024-02-19 23:48:59 -0500

    Project Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        String trash = ""; //Bad inputs (Strings)
+        // Create a do while loop to test for valid number
+        while (true) {
+            //Prompt for input of temperature in Celsius
+            System.out.print("Enter Temperature in Celsius: ");
+            //Determine if input is valid
+            if (in.hasNextDouble()) {
+                double celsius = in.nextDouble();
+                if (celsius < -273.15) {
+                    System.out.println("The temperature you entered is below absolute zero, please enter a valid temperature.");
+                } else {
+                    //Formula C to F
+                    double F = (celsius * 9 / 5) + 32;
+                    System.out.println("Your temperature in Celsius, " + celsius + ", is equal to " + F + " degrees Fahrenheit");
+                    break;
+                }
+            } else {
+                trash = in.nextLine();
+                System.out.println("\nYour entered: " + trash);
+                System.out.println("Please enter a valid temperature.");
+                in.next();
+            }
+        }
+        in.close();
     }
 }
+
+
+
